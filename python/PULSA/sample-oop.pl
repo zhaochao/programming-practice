35176968500216c4d159e257e9f3446c7c1b7893
zhaochao <zhaochao1984@gmail.com>
2012-06-10 17:56:40 +0800

    -python PULSA-: sample perl script with OO.

---
+++ python/PULSA/sample-oop.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+package Server;
+use strict;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    $self->{IP} = shift;
+    $self->{HOSTNAME} = shift;
+    bless($self);
+
+    return $self;
+}
+
+sub set_ip {
+    my $self = shift;
+    $self->{IP} = shift;
+    
+    return $self->{IP};
+}
+
+sub set_hostname {
+    my $self = shift;
+    $self->{HOSTNAME} = shift;
+
+    return $self->{HOSTNAME};
+}
+
+sub ping {
+    my $self = shift;
+    my $extern_ip = shift;
+    my $self_ip = $self->{IP};
+    my $self_host = $self->{HOSTNAME};
+
+    print "Pinging $extern_ip from $self_ip ($self_host)\n";
+    return 0;
+}
+
+$server = Server->new('192.168.1.15', 'grumbly');
+$server->ping('192.168.1.20');
