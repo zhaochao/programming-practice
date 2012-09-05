6baa10addfbfb6202feb996b928c01c81e3fa576
zhaochao <zhaochao1984@gmail.com>
2012-09-05 21:39:25 +0800

    -JMKDT-: manipulate the screen.

--- JMKDT/main.c
+++ JMKDT/main.c
@@ -1,8 +1,13 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 // Made for JamesM's tutorials
 //
+
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
     // All our initialisation calls will go in here.
+    monitor_clear();
+    monitor_write("Hello world!");
     return 0xDEADBABA;
 } 
