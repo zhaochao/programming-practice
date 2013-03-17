b54b095f952cd22217037fd93c98efb87ac4aac1
zhaochao <zhaohcao1984@gmail.com>
2013-03-17 21:33:46 +0800

    -APUE-: fig5.4, copy stdin to stdout using getc and putc. fig5.5, copy stdin to stdout using fgets and fputs. fig5.11, print buffering for various standard I/O streams. fig5.12, demonstrate tmpnam and tmpfile functions. fig5.13, demonstrate tempnam function.

---
+++ apue/ch5/getcputc.c
@@ -0,0 +1,15 @@
+#include "apue.h"
+
+int main(void)
+{
+    int c;
+
+    while ((c = getc(stdin)) != EOF)
+        if (putc(c, stdout) == EOF)
+            err_sys("output error");
+
+    if (ferror(stdin))
+        err_sys("intput error");
+
+    exit(0);
+}
---
+++ apue/ch5/tempnam.c
@@ -0,0 +1,12 @@
+#include "apue.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        err_quit("usage: %s <directory> <prefix>", argv[0]);
+
+    printf("%s\n", tempnam(argv[1][0] != ' ' ? argv[1] : NULL,
+        argv[2][0] != ' ' ? argv[2] : NULL));
+
+    exit(0);
+}
