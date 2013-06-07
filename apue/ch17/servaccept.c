8bc025f1a48d08e160656ecb34c189bb1d8f127c
zhaochao <zhaohcao1984@gmail.com>
2013-06-07 14:12:02 +0800

    -APUE-: fig17.10, the serv_listen function using STREAMS pipes. fig17.11, the serv_accept function using STREAMS pipes. fig17.12, the cli_conn function using STREAMS pipes.

---
+++ apue/ch17/servaccept.c
@@ -0,0 +1,17 @@
+#include "apue.h"
+#include <stropts.h>
+
+/*
+ * Wait for a client connection to arrive, and accept it.
+ * We alsa obtain the client's user ID.
+ * Returns new fd if all OK, < 0 on error.
+ */
+int serv_accept(int listenfd, uid_t *uidptr)
+{
+    struct strrecvfd recvfd;
+    if (ioctl(listenfd, I_RECVFD, &recvfd) < 0)
+        return(-1);     /* could be EINTR if signal caught */
+    if (uidptr != NULL)
+        *uidptr = recvfd.uid;   /* effective uid of the caller */
+    return(recvfd.fd);      /* return the new descriptor */
+}
